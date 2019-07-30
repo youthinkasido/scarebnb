1b348304f1abd2fe1d59f0c7bb7755d8f6c883e1
Arshia <youthinkasido@gmail.com>
2019-07-29 20:07:52 -0400

    hi

--- app/models/amenity.rb
+++ app/models/amenity.rb
@@ -1,7 +1,7 @@
 class Amenity < ApplicationRecord
-    validates :name, :created, presence: true
+#     validates :name, :created, presence: true
 
-    has_many :spot_amenities,
-    foreign_key: :amenity_id
+#     has_many :spot_amenities,
+#     foreign_key: :amenity_id
 
 end
--- app/models/spot_amenity.rb
+++ app/models/spot_amenity.rb
@@ -1,11 +1,11 @@
 class SpotAmenity < ApplicationRecord
-validates :spot_id, :amenity_id, presence: true
+# validates :spot_id, :amenity_id, presence: true
 
-belongs_to :amenity,
-foreign_key: amenity_id 
+# belongs_to :amenity,
+# foreign_key: amenity_id 
 
-belongs_to :spot,
-foreign_key: spot_id 
+# belongs_to :spot,
+# foreign_key: spot_id 
 
 
 end
