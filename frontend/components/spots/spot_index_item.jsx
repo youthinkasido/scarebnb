0c500b8f39d213f3374ee26c9a4b27c8541d9185
Arshia <youthinkasido@gmail.com>
2019-08-02 17:00:30 -0400

    spots

--- frontend/components/spots/spot_index_item.jsx
+++ frontend/components/spots/spot_index_item.jsx
@@ -5,16 +5,12 @@ import { Link } from 'react-router-dom';
 const SpotIndexItem = ({spot}) => (
     
     <li className="spot-index-item">
-        
         <Link to={`/spots/${spot.id}`}>
-
-            
             {/* <span>{spot.name}</span> */}
             <img className="spot-index-item-photo" src={spot.image_url} />
             <span>{spot.description}</span>
             <span>{spot.cost_per_night}</span>
             <span>{spot.rating}</span>
-
         </Link>
     </li>
 )
